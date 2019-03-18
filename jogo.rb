f90a929200d0cc79100ec7800aff68ba60de969b
Gabriel Castelo Garcia <866935@PPC01INFLL00324.pucminas.net>
2019-03-18 19:43:03 -0300

    #1: mensagem.

--- jogo.rb
+++ jogo.rb
@@ -1,24 +1,50 @@
 def da_boas_vindas
-	puts "Bem vindo ao jogo de adivinhacao"
-	puts "Qual seu nome?"
-	nome = gets
-	puts "\n\n\n"
-	puts "Comecaremos o jogo agora, #{nome}"
+		puts "Bem-vindo ao jogo de adivinhacao"
+		puts "Qual o seu nome?"
+		nome = gets
+		puts "\n\n\n\n"
+		puts "Comecaremos o jogo #{nome}"
 end
 
-def sorteia_numero_secreto
-	puts "Escolhendo um numero secreto entre 0 e 200..."
-	numero_secreto = 121
+def pede_dificuldade
+	puts "Qual o nivel de dificuldade que deseja? (1- facil, 5- dificil)"
+	dificuldade = gets.to_i
+end
+
+def sorteia_numero_secreto (dificuldade)
+	case dificuldade
+	when 1
+		maximo = 30
+	when 2
+		maximo = 60
+	when 3
+		maximo = 100
+	when 4
+		maximo = 150
+	when 5
+		maximo = 200
+	end
+
+	puts "Escolhendo um numero secreto entre 0 e #{maximo}"
+	numero_secreto = rand(maximo) + 1
+	puts "Adivinha o numero secreto a seguir"
+
 	numero_secreto
 end
 
-def pede_um_numero(tentativa, limite_de_tentativas)
+def pede_um_numero (chutes, tentativa, limite_de_tentativas)
 	puts "\n\n\n"
 	puts "Tentativa #{tentativa} de #{limite_de_tentativas}"
-	puts "Digite seu palpite: "
-	chute = gets
+	puts "Chutes ate agora#{chutes}"
+	puts "Entre com o numero"
+	chute = gets.strip.to_i
+	loop do
+		break unless chutes.include? chute
+		puts "Atencao, voce ja jogou o numero #{chute}!"
+		chute = gets.strip.to_i
+	end
 	puts "Voce chutou #{chute}"
-	chute.to_i
+	chute
 end
 
 def verifica_se_acertou(numero_secreto, chute)
@@ -37,23 +63,26 @@ def verifica_se_acertou(numero_secreto, chute)
 	end
 end
 
-def joga(limite_de_tentativas)
-	numero_secreto = sorteia_numero_secreto
+def joga(limite_de_tentativas, dificuldade)
+	chutes = []
+	numero_secreto = sorteia_numero_secreto dificuldade
 	(1..limite_de_tentativas).each do |tentativa|
-		chute = pede_um_numero tentativa, limite_de_tentativas
+		chute = pede_um_numero chutes, tentativa, limite_de_tentativas
+		chutes << chute
 		break if verifica_se_acertou numero_secreto, chute
 	end
 end
 
 def nao_quer_jogar?
-	puts "Deseja jogar novamente? (S/N)"
+	puts "Deseja jogar novamento? (S/N)"
 	quero_jogar = gets.strip.upcase
 	quero_jogar.casecmp("N").zero?
 end
 
 da_boas_vindas
 limite_de_tentativas = 3
+dificuldade = pede_dificuldade
 loop do
-	joga limite_de_tentativas
+	joga limite_de_tentativas, dificuldade
 	break if nao_quer_jogar?
 end
